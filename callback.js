b69781ef005d9ff4767a2203861e6e8036707b5b
Sergio Rios <serggio.rs@gmail.com>
2022-11-30 12:07:16 -0300

    Example Callback - homework

---
+++ callback.js
@@ -0,0 +1,9 @@
+function callback(mensajeDevLights) {
+
+    console.log("Este es un callback:", mensajeDevLights);
+  }
+  function mensajeDevLights(){
+    const mensaje = "Hola DevLights";
+    return mensaje;
+  }
+  callback(mensajeDevLights());
